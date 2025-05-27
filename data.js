7a2132716c5c208ec359c475c62624e2b5152fa4
Wix55 <58708292+Wix55@users.noreply.github.com>
2025-05-27 10:04:30 +0800

    UploadingContent

---
+++ data.js
@@ -0,0 +1,83 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-pxl_20250527_003943368pano_1",
+      "name": "PXL_20250527_003943368.PANO_1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 1865,
+      "initialViewParameters": {
+        "yaw": 0.375455598288708,
+        "pitch": 0,
+        "fov": 1.3645810919886565
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.40961191454693946,
+          "pitch": -0.19479067744577705,
+          "rotation": 6.283185307179586,
+          "target": "1-image1"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 0.0863193916370193,
+          "pitch": -0.20015985062587305,
+          "title": "Title",
+          "text": "Text"
+        }
+      ]
+    },
+    {
+      "id": "1-image1",
+      "name": "image1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 805.5,
+      "initialViewParameters": {
+        "pitch": 0,
+        "yaw": 0,
+        "fov": 1.5707963267948966
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": false
+  }
+};
